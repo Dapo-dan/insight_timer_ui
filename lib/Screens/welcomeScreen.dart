f6f5c8d84fc9cb3577d24ad354f9055da7178b31
Dapo <odapo138@gmail.com>
2022-01-29 23:39:09 +0100

    welcome page

---
+++ lib/Screens/welcomeScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+              'Join our community of 19 million people building healthy habits together'),
+          TextButton(
+            child: Text(
+              "Create an account",
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: null,
+          ),
+          TextButton(
+            child: Text(
+              "Already a member?",
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: null,
+          )
+        ],
+      ),
+    ));
+  }
+}
