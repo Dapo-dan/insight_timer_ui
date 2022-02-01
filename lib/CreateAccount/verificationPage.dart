f5275957e0a0998e6f414bbd85d63dc9756aaf89
Dapo <odapo138@gmail.com>
2022-02-01 10:50:03 +0100

    verification page

--- lib/CreateAccount/verificationPage.dart
+++ lib/CreateAccount/verificationPage.dart
@@ -6,91 +6,62 @@ class VerificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[600],
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-        margin: const EdgeInsets.only(
-          top: 200,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                const Icon(Icons.arrow_back),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Icon(Icons.mail),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Show us it's you",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 30),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text('Password'),
-                const SizedBox(
-                  height: 5,
-                ),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Divider(
-                      thickness: 1,
-                      color: Colors.grey,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text('or'),
-                    SizedBox(width: 5),
-                    Divider(
-                      thickness: 5,
-                      height: 10,
-                      color: Colors.grey,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-              ],
+        backgroundColor: Colors.grey[600],
+        body: Container(
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+            margin: const EdgeInsets.only(
+              top: 200,
             ),
-          ],
-        ),
-      ),
-    );
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const Icon(Icons.arrow_back),
+              const SizedBox(
+                height: 20,
+              ),
+              Column(
+                children: const [
+                  Icon(Icons.mail),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    "Show us it's you",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Please verify your email to continue",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+                  SizedBox(height: 50),
+                  TextField(
+                    autofillHints: <String>[],
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 70,
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      "Didn't get an email? Resend.",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+            ])));
   }
 }
