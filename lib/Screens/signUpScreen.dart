6e4574aed4a925875fde4a1eb86871e205bcb292
Dapo <odapo138@gmail.com>
2022-02-01 01:04:57 +0100

    Signup screen

--- lib/Screens/signUpScreen.dart
+++ lib/Screens/signUpScreen.dart
@@ -8,31 +8,43 @@ class SignUpScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[600],
       body: Container(
-        padding: const EdgeInsets.only(top: 50),
-        margin: const EdgeInsets.fromLTRB(10, 25, 10, 10),
-        color: Colors.white,
+        padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+        margin: const EdgeInsets.only(
+          top: 200,
+        ),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
         ),
         child: ListView(
-          padding: const EdgeInsets.all(8),
+          padding: const EdgeInsets.all(15),
           children: [
             Column(
               children: [
                 Row(
                   children: const [
-                    Text('Sign Up'),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 25),
+                    ),
                     Spacer(),
                     Icon(Icons.cancel)
                   ],
                 ),
                 const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Email',
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(
                   height: 5,
                 ),
-                const Text('Email'),
                 const TextField(
                   obscureText: true,
                   decoration: InputDecoration(
+                    fillColor: Colors.white,
                     border: OutlineInputBorder(),
                   ),
                 ),
@@ -45,7 +57,30 @@ class SignUpScreen extends StatelessWidget {
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                   ),
-                )
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                      minimumSize: const Size(400, 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      primary: Colors.grey,
+                      onSurface: Colors.grey,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignUpScreen()),
+                      );
+                    }),
               ],
             ),
           ],
