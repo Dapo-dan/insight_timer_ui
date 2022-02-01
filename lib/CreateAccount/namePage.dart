9fa379216075ae05c6bc36471ad14fca0a1a4f08
Dapo <odapo138@gmail.com>
2022-02-01 11:02:40 +0100

    name page

--- lib/CreateAccount/namePage.dart
+++ lib/CreateAccount/namePage.dart
@@ -1,1 +1,50 @@
 import 'package:flutter/material.dart';
+
+class NamePage extends StatelessWidget {
+  const NamePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(
+            "What's your name?",
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "We prefer using real names at Insight Timer. It's a trust thing",
+            style: TextStyle(fontSize: 15, color: Colors.grey),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: const [
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
